df1386ae9a3127d81718ed0f9bc6e6dd7b05fc82
Mai <mai.tran1@bytedance.com>
2026-05-23 07:45:15 -0700

    Initial Commit

---
+++ HackerReader/HackerReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerReaderApp.swift
+//  HackerReader
+//
+//  Created by ByteDance on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct HackerReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
