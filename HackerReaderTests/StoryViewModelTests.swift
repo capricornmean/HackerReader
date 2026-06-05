aa7179e00bd938a8ca003065688a987a7a5c0a90
Mai <hanonhoushow@gmail.com>
2026-06-05 16:40:44 -0700

    load more draft

--- HackerReaderTests/StoryViewModelTests.swift
+++ HackerReaderTests/StoryViewModelTests.swift
@@ -24,9 +24,23 @@ import Testing
         }
         await vm.loadInitial()
         #expect(snapshotMidFlight.map(\.id) == [1])
-        
         #expect(vm.stories.map(\.id) == [2, 3])
         #expect(storage.savedCalls.count == 1)
         #expect(storage.savedCalls.first?.map(\.id) == [2, 3])
+    }
+    
+    @Test func loadMore() async {
+        let storage = FakeStoryStorage()
+        let service = FakeStoryService()
+        service.cannedTopIDs = [1, 2, 3]
+        service.cannedStories = [GeneralHelpers.makeStory(id: 1)]
+        let vm = StoryViewModel(service: service, storage: storage)
+        await vm.loadInitial()
+        
+//        service.cannedTopIDs = [1, 2, 3]
+        service.cannedStories = [GeneralHelpers.makeStory(id: 1), GeneralHelpers.makeStory(id: 2), GeneralHelpers.makeStory(id: 3)]
+        await vm.loadMore()
+        #expect(storage.savedCalls.count == 2)
+        #expect(storage.savedCalls.last?.map(\.id) == [2, 3])
     }
 }
--- HackerReaderTests/TestHelpers/FakeStoryStorage.swift
+++ HackerReaderTests/TestHelpers/FakeStoryStorage.swift
@@ -5,16 +5,16 @@
 //  Created by mai on 6/3/26.
 //
 
-@MainActor class FakeStoryStorage: StoryStorageProtocol {
+class FakeStoryStorage: StoryStorageProtocol {
     var cannedCached: [Story] = []
     var savedCalls: [[Story]] = []
     
     func fetchCached() async -> [Story] {
-        return await cannedCached
+        return cannedCached
     }
     
     func fetchCached(id: Int) async -> Story? {
-        return await cannedCached.first(where: { $0.id == id })
+        return cannedCached.first(where: { $0.id == id })
     }
     
     func save(_ stories: [Story]) async {
